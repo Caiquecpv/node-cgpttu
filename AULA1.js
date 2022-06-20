b80c9e9fcdd91e8b3cf44eb103002fb227708cb0
Caiquecpv <casiqueira956@outlook.com>
2022-06-20 13:52:41 -0300

    1 file added, 1 deleted

---
+++ AULA1.js
@@ -0,0 +1,41 @@
+// Pilhas
+class Pilha {
+  constructor() {
+    this.itens = [];
+  }
+
+  push(item) {
+    this.itens.push(item);
+  }
+
+  pop() {
+    this.itens.pop();
+  }
+
+  top() {
+    return this.itens[this.itens.length - 1];
+  }
+
+  isEmpty() {
+    return this.itens.length == 0;
+  }
+}
+
+const pilha = new Pilha();
+pilha.push(10);
+pilha.push(20);
+pilha.push(30);
+pilha.push(40);
+pilha.push(50);
+pilha.push(60);
+
+pilha.pop();
+pilha.pop();
+pilha.pop();
+pilha.pop();
+pilha.pop();
+pilha.pop();
+pilha.pop();
+pilha.pop();
+
+console.table(pilha.isEmpty());
